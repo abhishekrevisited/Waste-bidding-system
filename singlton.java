b91b0043cc4ede5eedeaa2adfaad466eb0b03376
Abhishek <69053871+abhishekrevisited@users.noreply.github.com>
2020-08-14 16:18:52 +0530

    Create singlton.java

---
+++ singlton.java
@@ -0,0 +1,35 @@
+package io.com.didingapp.Volley;
+
+
+import java.util.ArrayList;
+
+import io.com.didingapp.history.historyModel;
+
+public class Singleton {
+    public static String firstName;
+    public static String lastname;
+    public static String mobile;
+    public static String email;
+    public static String img;
+    public static String password;
+    public static String balance;
+
+
+    public static String id;
+
+    public static String title;
+    public static String startDate;
+    public static String endDate;
+    public static String category;
+    public static String cat_id;
+    public static String auc_id;
+    public static String description;
+    public static String min_bids;
+
+    public static String status="0";
+
+
+    public static String isActive="0";
+    public static String pos;
+    public static historyModel historyModel;
+}
